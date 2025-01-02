c37b4b9ccb9597e09bfd11403dc060a591bad8c6
Vansh <vanshparate@gmail.com>
2025-01-02 23:54:53 +0530

    added cursor animations and cards

--- project-1/script.js
+++ project-1/script.js
@@ -44,3 +44,35 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+document.addEventListener("mousemove",function(dets){
+    gsap.to("#cursor",{
+        left: dets.x,
+        top: dets.y
+    })
+})
+
+document.querySelector("#child1").addEventListener("mouseenter",function(){
+    gsap.to("#cursor",{
+        transform: 'translate(-50%,-50%) scale(1)'
+    })
+})
+
+document.querySelector("#child1").addEventListener("mouseleave",function(){
+    gsap.to("#cursor",{
+        transform: 'translate(-50%,-50%) scale(1)'
+    })
+})
+
+document.querySelectorAll("child").forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        gsap.to("#cursor",{
+            transform: 'translate(-50%,-50%) scale(1)'
+        }) 
+    })
+    elem.addEventListener("mouseleave",function(){
+        gsap.to("#cursor",{
+            transform: 'translate(-50%,-50%) scale(0)'
+        }) 
+    })
+})
